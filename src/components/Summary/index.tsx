456c9dca7a2388b02af9edaed03e960d00475bb1
Tiago <tiagoamaralcbei@hotmail.com>
2023-03-13 10:49:27 -0300

    finalizado a interface visual

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,36 @@
+import { SummaryCard, SummaryContainer } from "./styles";
+
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
+
+export function Summary() {
+  return (
+    <SummaryContainer>
+      <SummaryCard>
+        <header>
+          <span>Entrandas</span>
+          <ArrowCircleUp size={32} color="#00b37e"/>
+        </header>
+
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+
+      <SummaryCard>
+        <header>
+          <span>Saídas</span>
+          <ArrowCircleDown size={32} color="#f75a68"/>
+        </header>
+
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+
+      <SummaryCard variant="green">
+        <header>
+          <span>Totaç</span>
+          <CurrencyDollar size={32} color="#fff"/>
+        </header>
+
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+    </SummaryContainer>
+  )
+}
